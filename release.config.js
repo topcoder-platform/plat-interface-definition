1804e9ef1da601abe295942c52a161beb583433d
liuliquan <lester.castro@wipro.com>
2023-10-17 03:45:56 -0500

    feat: add skills field for challenge (#49)
    * feat: add skills field for challenge
    
    * chore: setup github actions for release
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>
    
    * chore: update release config
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>
    
    * chore: update to latest actions versions
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>
    
    ---------
    
    Signed-off-by: Rakib Ansary <rakibansary@topcoder.com>
    Co-authored-by: Rakib Ansary <rakibansary@topcoder.com>

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  branches : ['main', {name : 'PLAT-*', channel : 'beta', prerelease : true}],
+  plugins :
+          [
+            '@semantic-release/commit-analyzer',
+            '@semantic-release/release-notes-generator',
+            '@semantic-release/changelog', '@semantic-release/npm',
+            [
+              '@semantic-release/github', {
+                assets :
+                [
+                  {path : 'dist/*.tar.gz', label : 'Binary'},
+                  {path : 'dist/*.zip', label : 'Compressed'}
+                ]
+              }
+            ]
+          ],
+  preset : 'angular',
+  parserOpts : {
+    preset : 'angular',
+    releaseRules :
+    [
+      {breaking : true, release : 'major'},
+      {type : 'feat', release : 'minor'},
+      {type : 'fix', release : 'patch'},
+      {type : 'chore', release : 'patch'},
+      {type : 'docs', release : 'patch'},
+    ]
+  }
+};
